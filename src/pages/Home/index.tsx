4dceae66c76aa85c1fe3ecc62f7e991dcdf015e7
Mihai Laurentiu Iordache <adiordache61@gmail.com>
2023-03-15 23:23:54 +0100

    - Added card to homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { Card } from "flowbite-react";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <div className="flex justify-center align-center">
+        <Card
+          className="sm:w-10/12 md:w-4/12 mt-10"
+          imgSrc="https://www.parliament.uk/contentassets/aa8b9933d3cb4364b827e7a60ea898e0/hl_library_roger-harris2022.jpg"
+        >
+          <p>Online library management</p>
+        </Card>
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
