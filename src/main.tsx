c3f175bfe599fb73a13dc28902cc3c1bb0609943
Mihai Laurentiu Iordache <adiordache61@gmail.com>
2023-03-21 23:33:58 +0100

    - Added react-query - Added axios

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import App from "./App";
 import "./index.css";
 
 import "devextreme/dist/css/dx.light.css";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
